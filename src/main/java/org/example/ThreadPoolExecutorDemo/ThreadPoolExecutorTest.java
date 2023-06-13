9ce27ca265bf28d4a72b54a6c034c83876ec7e86
huangxb <hxbwork2022@163.com>
2023-06-13 22:56:17 +0800

    1

--- src/main/java/org/example/ThreadPoolExecutorDemo/ThreadPoolExecutorTest.java
+++ src/main/java/org/example/ThreadPoolExecutorDemo/ThreadPoolExecutorTest.java
@@ -8,12 +8,17 @@ import java.util.concurrent.*;
  * @apiNote
  */
 public class ThreadPoolExecutorTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         ThreadPoolExecutor threadPoolExecutor1 = new ThreadPoolExecutor(2, 2, 0L, TimeUnit.SECONDS, new SynchronousQueue<Runnable>(),new ThreadPoolExecutor.DiscardOldestPolicy());
-        ExecutorService threadPoolExecutor = Executors.newFixedThreadPool(2);
+        //ExecutorService threadPoolExecutor = Executors.newFixedThreadPool(2);
         threadPoolExecutor1.submit(()->{
             System.out.println("我是一个异步线程");
         });
+
+        Future<Integer> submit = threadPoolExecutor1.submit(() -> {
+            return 111;
+        });
+        System.out.println(submit.get());
     }
 }
